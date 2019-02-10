b3aeea7adfc6b63a5f2cddb35160eebb4dcf9f11
Benjamin Häublein <benjaminh@debian.vm.hp>
2019-02-10 17:17:17 +0100

    added openapi/server-bundle

--- Symfony-API/config/bundles.php
+++ Symfony-API/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
