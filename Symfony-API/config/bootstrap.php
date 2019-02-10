abed17c785e0f0abe0ada3384387a631a1c5f502
Benjamin Häublein <benjaminh@debian.vm.hp>
2019-02-10 17:13:17 +0100

    add AnnotationsRegistry

--- Symfony-API/config/bootstrap.php
+++ Symfony-API/config/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\Dotenv\Dotenv;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
@@ -19,3 +20,5 @@ if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
+AnnotationRegistry::registerLoader('class_exists');
+
