287d26400a01cc085d10ed711ee06695bdda511a
Benjamin Häublein <benjaminh@debian.vm.hp>
2019-02-23 18:11:00 +0100

    Add all account functionality

--- Symfony-API/src/Api/AccountsApi.php
+++ Symfony-API/src/Api/AccountsApi.php
@@ -1,14 +1,50 @@
 <?php
-// src/Acme/MyBundle/Api/AccountsApiInterface.php
 
 namespace App\Api;
 
+use App\Entity\Account;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Common\Collections\Criteria;
 use OpenAPI\Server\Api\AccountsApiInterface;
-use OpenAPI\Server\Model\Account;
+use OpenAPI\Server\Model\Account as OpenAPIAccount;
+use OpenAPI\Server\Model\AccountId;
+use OpenAPI\Server\Model\Index;
+use Symfony\Component\Security\Core\Security;
 
 class AccountsApi implements AccountsApiInterface
 {
+    private $entityManager;
+    private $passwordEncoder;
+    private $security;
+    private $session;
 
+    public function __construct(EntityManagerInterface $entityManager, Security $security)
+    {
+        $this->entityManager = $entityManager;
+        $this->security = $security;
+    }
+
+    private function getCurrentUsersAccounts() 
+    {
+        $currentUser = $this->security->getUser();
+	return $currentUser->getAccounts()->map(function($account) { return $account->getAsOpenAPIAccount(); } );
+    }
+
+    private function getCurrentUsersSpecificAccount(int $id)
+    {
+        $currentUser = $this->security->getUser();
+        $idCriteria = Criteria::create()
+            ->where(Criteria::expr()->eq("id", $id));
+        return $currentUser->getAccounts()->matching($idCriteria)[0];
+    }
+
+    private function fillAccountFromRequest($request, Account $account)
+    {
+        $account->setName($request->getName());
+        $account->setPassword($request->getPassword());
+        $account->setOther($request->getAdditional());
+    }
+    
     // ...
 
     /**
@@ -16,13 +52,36 @@ class AccountsApi implements AccountsApiInterface
      */
     public function getAccounts(&$responseCode, array &$responseHeaders)
     {
-        $accounts = array();
-        for ($i = 0; $i <= 10; $i++) {
-            array_push($accounts, new Account(["name"=> "test" . $i ]));
-        }
-	return $accounts;
-        // Implement the operation ...
+        return $this->getCurrentUsersAccounts();
     }
+
+    public function addAccount(OpenAPIAccount $request, &$responseCode, array &$responseHeaders)
+    {
+        $currentUser = $this->security->getUser();
+        $account = new Account();
+        $this->fillAccountFromRequest($request, $account);
+        $currentUser->addAccount($account);
+        $this->entityManager->persist($account);
+        $this->entityManager->flush();
+        return $this->getCurrentUsersAccounts();
+    }
+
+    public function deleteAccount(Index $body, &$responseCode, array &$responseHeaders)
+    {
+        $account = $this->getCurrentUsersSpecificAccount($body->getIndex());
+        $this->entityManager->remove($account);
+        $this->entityManager->flush();
+        return $this->getCurrentUsersAccounts();
+    }
+
+    public function updateAccount(AccountId $body, &$responseCode, array &$responseHeaders)
+    {
+        $account = $this->getCurrentUsersSpecificAccount($body->getIndex());
+        $this->fillAccountFromRequest($body, $account);
+        $this->entityManager->flush();
+        return $this->getCurrentUsersAccounts();
+    }
+
 
     // ...
 }
