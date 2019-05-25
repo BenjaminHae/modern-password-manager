a20dddc11ac55b922c43d30abb0d375a7759d77b
Benjamin Häublein <benjaminh@debian.vm.hp>
2019-05-25 09:37:24 +0200

    add userid

--- dockerBuild/generateOpenAPIModules.sh
+++ dockerBuild/generateOpenAPIModules.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ..
 # symfony
-docker run --user 1000:1000 --rm -v ${PWD}:/local openapitools/openapi-generator-cli:latest generate -i /local/OpenAPIDescription.yaml     -g php-symfony -o /local/OpenAPIServerBundle
+docker run --user ${UID}:${GID} --rm -v ${PWD}:/local openapitools/openapi-generator-cli:latest generate -i /local/OpenAPIDescription.yaml     -g php-symfony -o /local/OpenAPIServerBundle
 # angular
-docker run --user 1000:1000 --rm -v ${PWD}:/local openapitools/openapi-generator-cli:latest generate -i /local/OpenAPIDescription.yaml     -g typescript-angular -o /local/OpenAPIAngularClient --additional-properties npmName=@pm-server/pm-server
+docker run --user ${UID}:${GID} --rm -v ${PWD}:/local openapitools/openapi-generator-cli:latest generate -i /local/OpenAPIDescription.yaml     -g typescript-angular -o /local/OpenAPIAngularClient --additional-properties npmName=@pm-server/pm-server
