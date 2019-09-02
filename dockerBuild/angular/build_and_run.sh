650d0774e639e084c44ff4f21d400460ecb6e1b2
Benjamin Häublein <benjaminh@debian.vm.hp>
2019-09-02 18:47:31 +0200

    add build to angular build docker

--- dockerBuild/angular/build_and_run.sh
+++ dockerBuild/angular/build_and_run.sh
@@ -10,4 +10,5 @@ npm install
 rm -rf "node_modules/@pm-server/pm-server"
 cp -r "../OpenAPIAngularClient/dist" "node_modules/@pm-server/pm-server"
 rm -r "node_modules/@pm-server/pm-server/node_modules"
+ng build --prod
 ng serve --host 0.0.0.0 --disable-host-check
