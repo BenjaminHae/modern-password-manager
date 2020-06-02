495bc96bf792d74a82096674144c149b1a65904d
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-06-02 20:30:54 +0200

    add /user/settings and /user/history endpoints

--- reactClient/my-app/src/components/Authenticated/Authenticated.tsx
+++ reactClient/my-app/src/components/Authenticated/Authenticated.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Authenticated.module.css';
 import { Account } from '../../backend/models/account';
+import AccountList from '../AccountList/AccountList';
 
 interface AuthenticatedProps {
 	accounts: Array<Account>;
@@ -9,9 +10,13 @@ interface AuthenticatedState {
 }
 class Authenticated extends React.Component<AuthenticatedProps, AuthenticatedState> {
   render () {
+    let accounts = this.props.accounts.map((item, key) =>
+	<li key={item.index}>{item.name}</li>
+	);
     return (
 	  <div className={styles.Authenticated}>
 	    Authenticated Component
+		<AccountList accounts={this.props.accounts} />
 	  </div>
 	);
   }
