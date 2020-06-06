0c7712a5bacffa57e3284b069a163f594a0e7a42
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-06-06 15:00:48 +0200

    add static page which will later become react

--- Symfony-API/config/bundles.php
+++ Symfony-API/config/bundles.php
@@ -9,4 +9,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
