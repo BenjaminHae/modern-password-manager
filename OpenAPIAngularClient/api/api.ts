5b74510c9527dc1074645573e28b1168ca6e5c6d
Benjamin Häublein <benjaminhaeublein@gmail.com>
2020-06-23 13:34:54 +0200

    remove OpenAPIAngularClient

--- OpenAPIAngularClient/api/api.ts
+++
@@ -1,7 +0,0 @@
-export * from './accounts.service';
-import { AccountsService } from './accounts.service';
-export * from './maintenance.service';
-import { MaintenanceService } from './maintenance.service';
-export * from './user.service';
-import { UserService } from './user.service';
-export const APIS = [AccountsService, MaintenanceService, UserService];
