defdad43e0428e48920c7460e3305455b1d55f3d
Benjamin Häublein <benjaminhaeublein@gmail.com>
2020-06-25 22:00:52 +0200

    add cleanUp to ICredentialProviderImplementations

--- reactClient/my-app/src/backend/controller/credentialProviderPassword.ts
+++ reactClient/my-app/src/backend/controller/credentialProviderPassword.ts
@@ -35,4 +35,8 @@ export class CredentialProviderPassword implements ICredentialProvider {
     }
     return this.key;
   }
+
+  cleanUp(): Promise<void>{
+    return Promise.resolve();
+  }
 }
--- reactClient/my-app/src/backend/controller/credentialProviderUndefined.ts
+++ reactClient/my-app/src/backend/controller/credentialProviderUndefined.ts
@@ -3,4 +3,8 @@ export class CredentialProviderUndefined {
   getKey(): CryptoKey{
     throw new Error("No Credentials existing");
   }
+
+  cleanUp(): Promise<void> {
+    return Promise.resolve();
+  }
 }
