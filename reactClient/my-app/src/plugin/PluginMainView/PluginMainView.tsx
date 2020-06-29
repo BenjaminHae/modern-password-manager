ac06f5df1deae596132766261664dd63c2a43bc1
Benjamin Häublein <benjaminhaeublein@gmail.com>
2020-06-29 15:53:57 +0200

    start plugin system

---
+++ reactClient/my-app/src/plugin/PluginMainView/PluginMainView.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './PluginMainView.module.css';
+import { BackendService } from '../../../backend/backend.service';
+
+interface PluginMainViewProps {
+  backend: BackendService;
+}
+interface PluginMainViewState {
+}
+class PluginMainView extends React.Component<PluginMainViewProps, PluginMainViewState> {
+  constructor(props: PluginMainViewProps) {
+    super(props);
+  }
+  render () {
+    return (
+      <div className={styles.PluginMainView}>
+      </div>
+    );
+  }
+}
+export default PluginMainView;
---
+++ reactClient/my-app/src/plugin/PluginSystem.tsx
@@ -0,0 +1,12 @@
+import { Account } from '../backend/models/account';
+
+export class PluginSystem {
+
+  registerPlugin() {
+  }
+
+  async callHook<InputData,OutputData=void>(data:InputData): Promise<OutputData> {
+
+  }
+
+}
