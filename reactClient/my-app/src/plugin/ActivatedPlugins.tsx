1fd50fe75d3dac60d746a39da42c9e61a36f0193
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-07-02 08:29:49 +0200

    add plugin for copying passwords to clipboard

--- reactClient/my-app/src/plugin/ActivatedPlugins.tsx
+++ reactClient/my-app/src/plugin/ActivatedPlugins.tsx
@@ -1,5 +1,6 @@
 import { PluginSystem } from './PluginSystem';
 import { BasePlugin } from './BasePlugin';
 import { TagsPlugin } from './TagsPlugin/Tags';
+import { CopyPlugin } from './CopyPlugin/Copy';
 
-export const ActivatedPlugins:Array<new (pluginSystem: PluginSystem) => BasePlugin> = [TagsPlugin];
+export const ActivatedPlugins:Array<new (pluginSystem: PluginSystem) => BasePlugin> = [TagsPlugin, CopyPlugin];
---
+++ reactClient/my-app/src/plugin/CopyPlugin/Copy.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Account } from '../../backend/models/account';
+import { AccountTransformerService } from '../../backend/controller/account-transformer.service';
+import { BasePlugin, IPluginWithAccountList, IPluginRequiresTransformer } from '../BasePlugin';
+import { IDataTableColumn } from 'react-data-table-component';
+import Button from 'react-bootstrap/Button';
+
+export class CopyPlugin extends BasePlugin implements IPluginWithAccountList, IPluginRequiresTransformer {
+  transformer?: AccountTransformerService;
+ 
+  setTransformer(transformer: AccountTransformerService) {
+    this.transformer = transformer;
+  }
+  accountList(column: IDataTableColumn<Account>): IDataTableColumn<Account> {
+    if (column.name === "Password") {
+      if (column.cell !== undefined) {
+        let oldCell = column.cell;
+        column.cell = (row: Account) => {
+          return [oldCell(row),this.showCopyPassword(row)]
+        }
+      }
+      else {
+        return (row: Account) => this.showCopyPassword(row)
+      }
+    }
+    return column;
+  }
+  showCopyPassword(account: Account) {
+    console.log("showCopyPassword");
+    if(!navigator.clipboard) {
+      return;
+    }
+    return (<Button onClick={() => this.copyPassword(account)}>Copy</Button>)
+  }
+  async copyPassword(account: Account) {
+    if (!this.transformer)
+      return;
+    navigator.clipboard.writeText(await this.transformer.getPassword(account))
+  }
+}
+
