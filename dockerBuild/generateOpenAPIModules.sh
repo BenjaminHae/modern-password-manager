69600e97a52a53aa73f82b48f33eb7c1bc44ac7e
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-07-06 20:59:52 +0200

    explicitly specify version of OpenAPI-generator

--- dockerBuild/generateOpenAPIModules.sh
+++ dockerBuild/generateOpenAPIModules.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cd ..
 # symfony
-docker run --userns=host --user ${UID}:${GID} --rm -v ${PWD}:/local openapitools/openapi-generator-cli:latest generate -i /local/OpenAPIDescription.yaml     -g php-symfony -o /local/OpenAPIServerBundle
+GENERATOR=openapitools/openapi-generator-cli:v5.0.0-beta
+
+docker run --userns=host --user ${UID}:${GID} --rm -v ${PWD}:/local "${GENERATOR}" generate -i /local/OpenAPIDescription.yaml     -g php-symfony -o /local/OpenAPIServerBundle
 # react
-docker run --userns=host --user ${UID}:${GID} --rm -v ${PWD}:/local openapitools/openapi-generator-cli:latest generate -i /local/OpenAPIDescription.yaml     -g typescript-fetch -o /local/OpenAPIReactClient --additional-properties npmName=@pm-server/pm-server-react-client
+docker run --userns=host --user ${UID}:${GID} --rm -v ${PWD}:/local "${GENERATOR}" generate -i /local/OpenAPIDescription.yaml     -g typescript-fetch -o /local/OpenAPIReactClient --additional-properties npmName=@pm-server/pm-server-react-client
