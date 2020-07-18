f2e41b967bfe72fff1ad10e1944908f3885da103
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-07-18 15:02:38 +0200

    styling of PLuginMainView

--- reactClient/my-app/src/plugin/PluginMainView/PluginMainView.tsx
+++ reactClient/my-app/src/plugin/PluginMainView/PluginMainView.tsx
@@ -3,6 +3,8 @@ import styles from './PluginMainView.module.css';
 //import { BackendService } from '../../backend/backend.service';
 import { PluginSystem } from '../PluginSystem';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 interface PluginMainViewProps {
 //  backend: BackendService,
@@ -13,10 +15,14 @@ interface PluginMainViewState {
 class PluginMainView extends React.Component<PluginMainViewProps, PluginMainViewState> {
   render () {
     return (
-      <div className={styles.PluginMainView}>
-        { this.props.pluginSystem.getMainView() }
-        { this.props.pluginSystem.filterPresent && <Button onClick={()=> {this.props.pluginSystem.clearFilters()}}>Clear Filters</Button> }
-      </div>
+      <Container className={styles.PluginMainView}>
+        <Row>
+          { this.props.pluginSystem.getMainView() }
+        </Row>
+        <Row>
+          { this.props.pluginSystem.filterPresent && <Button className={styles.ResetFilter} onClick={()=> {this.props.pluginSystem.clearFilters()}}>Clear Filters</Button> }
+        </Row>
+      </Container>
     );
   }
 }
