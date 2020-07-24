a34d05129a5c70db439f99e5538542d5ab305b74
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-07-24 18:15:40 +0200

    add form validation

--- reactClient/my-app/src/components/Login/Login.tsx
+++ reactClient/my-app/src/components/Login/Login.tsx
@@ -9,47 +9,66 @@ interface LoginProps {
   doLogin: (username: string, password: string) => void;
   ready: boolean;
 }
-interface LoginState {
+interface LoginFormValues {
   username: string;
   password: string;
+}
+interface LoginState extends LoginFormValues{
   waiting: boolean;
+  validated: boolean;
 }
 
 class Login extends React.Component<LoginProps, LoginState> {
+  readonly emptyInput = {
+    username: "",
+    password: "",
+  }
   constructor(props: LoginProps) {
     super(props);
-    this.state = { username: '', password: '', waiting: false };
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.state = { 
+      ...this.emptyInput, 
+      waiting: false,
+      validated: false
+    };
+    this.handleGenericChange = this.handleGenericChange.bind(this);
     this.doLogon = this.doLogon.bind(this);
   }
-  handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({username: event.target.value});
+  handleGenericChange(event: React.ChangeEvent<HTMLInputElement>) {
+    if (event.target.name in (this.state as LoginFormValues)) {
+      this.setState({[event.target.name]: event.target.value} as Pick<LoginFormValues, keyof LoginFormValues>);
+    }
   }
-  handlePasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({password: event.target.value});
-  }
-  async doLogon(event: React.FormEvent) {
+  async doLogon(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    if (!event.currentTarget.checkValidity()) {
+      this.setState({validated: true});
+      return;
+    }
     let password = this.state.password;
     this.setState({ password: "", waiting: true });
-    await this.props.doLogin(this.state.username, password)
-    this.setState({ waiting: false });
+    await this.props.doLogin(this.state.username, password);
+    this.setState({ waiting: false, validated: false });
   }
   render () {
     return (
 	  <div className={styles.Login}>
       <Col lg={{ span: 2, offset: 5 }} md={{ span: 4, offset: 4 }} sm={{ span: 10, offset: 1 }}>
         <h2>Login</h2>
-        <Form onSubmit={this.doLogon}>
+        <Form onSubmit={this.doLogon} noValidate validated={this.state.validated} >
           <fieldset disabled={this.state.waiting}>
             <Form.Group controlId="formUsername">
               <Form.Label>Username</Form.Label>
-              <Form.Control type="text" placeholder="Enter Username" name="username" onChange={this.handleNameChange}/>
+              <Form.Control type="text" placeholder="Enter Username" name="username" onChange={this.handleGenericChange} required />
+              <Form.Control.Feedback type="invalid">
+                Username is required
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId="formPassword">
               <Form.Label>Password</Form.Label>
-              <PasswordInputWithToggle onChange={this.handlePasswordChange} value={this.state.password} />
+              <PasswordInputWithToggle onChange={this.handleGenericChange} value={this.state.password} required />
+              <Form.Control.Feedback type="invalid">
+                Password is required
+              </Form.Control.Feedback>
             </Form.Group>
             <Button disabled={!this.props.ready} variant="primary" type="submit">{ this.state.waiting || !this.props.ready ? "Wait" : "Login" }</Button>
           </fieldset>
