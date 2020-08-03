4fa1e5e312f0f1bac7c0882ce80239506e5ba50d
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-08-03 14:40:20 +0200

    more header styling

--- reactClient/my-app/src/App.tsx
+++ reactClient/my-app/src/App.tsx
@@ -243,7 +243,7 @@ export default class App extends React.Component<AppProps, AppState> {
 	      <header className="App-header">
           <Container fluid>
             <Row>
-              <Col xl={{ span: 4, offset: 4 }} lg={{ span: 6, offset: 3 }} md={{ span: 6, offset: 3 }} xs={{ span: 6, offset: 1 }} >
+              <Col xl={{ span: 4, offset: 4 }} lg={{ span: 6, offset: 3 }} md={{ span: 6, offset: 3 }} xs={{ span: 6, offset: 1 }} className="text-center">
                 Password Manager
               </Col>
               <Col xl={{ span: 2, offset: 2 }} lg={{ span: 2, offset: 1 }} md={{ span: 3, offset: 0 }} xs={{ span: 3, offset: 2 }} className="text-right">
