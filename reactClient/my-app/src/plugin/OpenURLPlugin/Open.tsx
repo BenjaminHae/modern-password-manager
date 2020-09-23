70333af666b0e5dce989bacb9f37745b37039ec1
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-09-23 11:14:22 +0200

    add checks against xss

--- reactClient/my-app/src/plugin/OpenURLPlugin/Open.tsx
+++ reactClient/my-app/src/plugin/OpenURLPlugin/Open.tsx
@@ -6,8 +6,19 @@ import { Globe } from 'react-bootstrap-icons';
 
 export default class OpenURLPlugin extends BasePlugin implements IPluginWithAccountButton {
 
+  private isSafe(url: string): boolean {
+    try {
+      const protocol = new URL(url).protocol;
+      return (protocol === "https:");
+    }
+    catch {
+      return false;
+    }
+  }
   accountButton(account: Account): void | JSX.Element{
     if (!account.other.url)
+      return
+    if (!this.isSafe(account.other.url))
       return
     return (<Button variant="secondary" as="a" href={account.other.url} target="blank" rel="noopener noreferrer"><Globe/></Button>)
   }
