5f8cbfd2a2eb4ce2a0f4fa2a0c2231394196ab7a
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-09-25 00:21:19 +0200

    hide TagsPlugin on mobile

--- reactClient/my-app/src/plugin/TagsPlugin/TagViewComponent/TagViewComponent.tsx
+++ reactClient/my-app/src/plugin/TagsPlugin/TagViewComponent/TagViewComponent.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './TagViewComponent.module.css';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
 interface TagViewProps {
   tags: Array<string>;
@@ -9,11 +10,12 @@ interface TagViewProps {
 }
 interface TagViewState {
   selectedTags: Array<string>;
+  show: boolean;
 }
 class TagViewComponent extends React.Component<TagViewProps, TagViewState> {
   constructor (props: TagViewProps) {
     super(props);
-    this.state = {selectedTags: []};
+    this.state = {selectedTags: [], show: false};
   }
   clearSelected(): void {
     this.setState({selectedTags: []});
@@ -37,9 +39,11 @@ class TagViewComponent extends React.Component<TagViewProps, TagViewState> {
   }
   render(): JSX.Element {
     return (
-        <Col className={styles.TagViewComponent} lg={3} xl={4} sm={6} xs={12}>
-          <h3>Tags</h3>
-          {this.printTags()}
+        <Col className={styles.TagViewComponent} xl={4} lg={4} sm={12} xs={12}>
+          <h3>Tags <Button size="sm" className="d-lg-none" onClick={()=>this.setState({show: !this.state.show})}>{this.state.show ? "Hide" : "Show"}</Button> </h3>
+          <Container className={this.state.show ? "" : "d-none d-lg-block"} >
+            {this.printTags()}
+          </Container>
         </Col>
         );
   }
