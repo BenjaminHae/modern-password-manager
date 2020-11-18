449325cb1d5cfccd3b910e93a3cfca7403fd1005
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-11-18 19:53:58 +0100

    implement registering new key with credentials

--- reactClient/my-app/src/components/WebAuthn/WebAuthn.tsx
+++ reactClient/my-app/src/components/WebAuthn/WebAuthn.tsx
@@ -1,49 +1,134 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './WebAuthn.module.css';
 import { UserWebAuthnCred } from '@pm-server/pm-server-react-client';
 import DataTable from 'react-data-table-component';
 import { IDataTableColumn } from 'react-data-table-component';
 import { Plus, Trash } from 'react-bootstrap-icons';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import PasswordInputWithToggle from '../PasswordInputWithToggle/PasswordInputWithToggle';
+import { IMessageOptions } from '../Message/Message';
 
 export interface IWebAuthnProps {
   webAuthnDevices: Array<UserWebAuthnCred>;
   webAuthnThisDeviceRegistered: boolean;
   webAuthnLoadHandler: () => Promise<void>;
-  webAuthnCreateCredHandler: () => Promise<void>;
+  webAuthnCreateCredHandler: (devicename: string, username: string, password: string) => Promise<void>;
   webAuthnDeleteCredHandler: (creds: UserWebAuthnCred) => Promise<void>;
+  showMessage: (message: string, options?: IMessageOptions) => void;
 }
-const WebAuthn: React.FC<IWebAuthnProps> = (props: IWebAuthnProps) => {
-  const columns: Array<IDataTableColumn> = [
-    {name: 'Device Name', selector: 'name'},
-    {name: 'Last Used', cell: () => <>not implemented</>},
-    { 
-      name: "",  
-      ignoreRowClick: true, 
-      cell: (row: UserWebAuthnCred) => <Button onClick={()=>{props.webAuthnDeleteCredHandler(row)}}><Trash/></Button>
-    }
-  ];
+interface WebAuthnFormValues {
+  username: string;
+  devicename: string;
+  password: string;
+}
+interface WebAuthnState extends WebAuthnFormValues{
+  showDialog: boolean;
+  columns: Array<IDataTableColumn>;
+}
+class WebAuthn extends React.Component<IWebAuthnProps, WebAuthnState> {
+  readonly emptyInput = {
+    username: "",
+    devicename: "",
+    password: ""
+  }
 
-  useEffect(() => {
+  constructor(props: IWebAuthnProps) {
+    super(props);
+    this.state = { 
+      ...this.emptyInput, 
+      showDialog: false,
+      columns: this.getColumns()
+    };
     if (props.webAuthnDevices.length === 0) {
       props.webAuthnLoadHandler();
     }
-  });
-
-  return (
+    this.handleGenericChange = this.handleGenericChange.bind(this);
+  }
+  getColumns(): Array<IDataTableColumn> {
+    return [
+      {name: 'Device Name', selector: 'name'},
+      {name: 'Last Used', cell: () => <>not implemented</>},
+      { 
+        name: "",  
+        ignoreRowClick: true, 
+        cell: (row: UserWebAuthnCred) => <Button onClick={()=>{this.props.webAuthnDeleteCredHandler(row)}}><Trash/></Button>
+      }
+    ];
+  }
+  handleGenericChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    if (event.target.name in (this.state as WebAuthnFormValues)) {
+      this.setState({[event.target.name]: event.target.value} as Pick<WebAuthnFormValues, keyof WebAuthnFormValues>);
+    }
+  }
+  setShowDialog = (state: boolean) => {
+    this.setState({showDialog: state});
+  }
+  handleDialogStore = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    //props.webAuthnCreateCredHandler()
+    try {
+      await this.props.webAuthnCreateCredHandler(this.state.devicename, this.state.username, this.state.password);
+      this.props.showMessage(`Successfully stored key for ${this.state.devicename}`, {variant : "info"});
+      this.handleDialogClose();
+    }
+    catch(e) {
+      this.props.showMessage(`Error when storing key: ${e.message}`, {autoClose: false, variant: "danger"});
+      throw(e);
+    }
+  }
+  handleDialogClose = () => {
+    this.setShowDialog(false);
+    this.setState(this.emptyInput);
+  }
+  handleDialogShow = () => this.setShowDialog(true);
+  
+  render (): JSX.Element {
+    return (
       <div className={styles.WebAuthn}>
         <h3>Login without password</h3>
-        {!props.webAuthnThisDeviceRegistered && 
-          <Button onClick={() => {props.webAuthnCreateCredHandler()}}><Plus/> Register this device </Button>
+        {!this.props.webAuthnThisDeviceRegistered && 
+          <Button onClick={this.handleDialogShow}><Plus/> Register this device </Button>
         }
-        {props.webAuthnDevices.length > 0 &&
+        <Modal show={this.state.showDialog} onHide={this.handleDialogClose} >
+          <Modal.Header closeButton>
+            <Modal.Title>Add Login Key</Modal.Title>
+          </Modal.Header>
+          <Form onSubmit={this.handleDialogStore}>
+            <Modal.Body>
+              <Form.Group controlId="webAuthnFormUsername">
+                <Form.Label>Username (to be shown in authentication dialog)</Form.Label>
+                <Form.Control type="text" autoFocus placeholder="Enter Username" name="username" onChange={this.handleGenericChange} required />
+              </Form.Group>
+              <Form.Group controlId="webAuthnFormDeviceName">
+                <Form.Label>Device name to identify this device</Form.Label>
+                <Form.Control type="text" autoFocus placeholder="Enter Device Name" name="devicename" onChange={this.handleGenericChange} required />
+              </Form.Group>
+              <Form.Group controlId="webAuthnFormPassword">
+                <Form.Label>Password (your current password)</Form.Label>
+                <PasswordInputWithToggle onChange={this.handleGenericChange} value={this.state.password} required />
+              </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.handleDialogClose}>
+                Close
+              </Button>
+              <Button variant="primary" type="submit">
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal>
+        {this.props.webAuthnDevices.length > 0 &&
           <div>
             <h4>Stored devices</h4>
-            <DataTable noHeader columns={columns} data={props.webAuthnDevices} dense pagination striped/>
+            <DataTable noHeader columns={this.state.columns} data={this.props.webAuthnDevices} dense pagination striped/>
           </div>
         }
       </div>
       )
-};
+  }
+}
 
 export default WebAuthn;
