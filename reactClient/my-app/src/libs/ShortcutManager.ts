b6e65822a8d258bb842a3b78c6479746967506e2
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-12-09 17:20:55 +0100

    allow escape everywhere

--- reactClient/my-app/src/libs/ShortcutManager.ts
+++ reactClient/my-app/src/libs/ShortcutManager.ts
@@ -1,5 +1,5 @@
 import React from 'react';
-import Mousetrap, { ExtendedKeyboardEvent }from 'mousetrap';
+import Mousetrap, { ExtendedKeyboardEvent, MousetrapInstance } from 'mousetrap';
 
 
 export interface ShortcutEntry {
@@ -11,34 +11,42 @@ export interface ShortcutEntry {
 
 export default class ShortcutManager {
   private shortcuts: Array<ShortcutEntry>;
+  private mousetrap: MousetrapInstance;
   
   constructor() {
+    this.mousetrap = new Mousetrap();
     this.shortcuts = [];
+    const originalStopCallback = this.mousetrap.stopCallback;
+    this.mousetrap.stopCallback = function(e, element, combo) {
+      if (combo == "esc")
+        return false;
+      return originalStopCallback(e, element, combo);
+    }
   }
 
   addShortcut(entry: ShortcutEntry) {
-    Mousetrap.bind(entry.shortcut, entry.action);
+    this.mousetrap.bind(entry.shortcut, entry.action);
     this.shortcuts.push(entry);
   }
 
   removeShortcut(shortcut: string): void {
     const index = this.shortcuts.findIndex((element) => element.shortcut === shortcut);
     if (index > -1) {
-      Mousetrap.unbind(shortcut);
+      this.mousetrap.unbind(shortcut);
       this.shortcuts.splice(index, 1);
     }
   }
 
   removeAllShortcuts(): void {
     for (let item of this.shortcuts) {
-      Mousetrap.unbind(item.shortcut);
+      this.mousetrap.unbind(item.shortcut);
     }
     this.shortcuts.length = 0;
   }
 
   removeByComponent(component: React.Component): void {
     for (let item of this.shortcuts.filter((element) => element.component === component)) {
-      Mousetrap.unbind(item.shortcut);
+      this.mousetrap.unbind(item.shortcut);
     }
     this.shortcuts = this.shortcuts.filter((element) => element.component !== component);
   }
