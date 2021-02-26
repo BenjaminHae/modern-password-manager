63102fc845af52806d9aa84ae7888cc2e92106c0
Benjamin Häublein <benjaminh@debian.vm.hp>
2021-02-26 20:32:17 +0100

    remove modal

--- reactClient/my-app/src/components/WebAuthn/WebAuthn.tsx
+++ reactClient/my-app/src/components/WebAuthn/WebAuthn.tsx
@@ -5,11 +5,13 @@ import DataTable from 'react-data-table-component';
 import { IDataTableColumn } from 'react-data-table-component';
 import { Plus, Trash } from 'react-bootstrap-icons';
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import PasswordInputWithToggle from '../PasswordInputWithToggle/PasswordInputWithToggle';
 import { IMessageOptions } from '../../libs/MessageManager';
 import { IWebAuthnProps } from '../commonProps';
+import Collapse from 'react-bootstrap/Collapse';
+import CloseButton from 'react-bootstrap/CloseButton';
+import Col from 'react-bootstrap/Col';
 
 interface WebAuthnFormValues {
   username: string;
@@ -95,41 +97,37 @@ class WebAuthn extends React.Component<IWebAuthnProps, WebAuthnState> {
         <p>You can store decryption keys on devices so that you don&apos;t have to enter your password on login.<br/>
           However access to this decryption key is protected by your device, for example through biometric authentication (fingerprint or face recognition).<br/>
           This explanation is quite simplified. For more information look at the documentation of this password manager.</p>
-          <Button onClick={this.handleDialogShow}><Plus/> Register current user on this device </Button>
+          <Collapse in={ !this.state.showDialog } >
+            <Button onClick={this.handleDialogShow}><Plus/> Register current user on this device </Button>
+          </Collapse>
         {this.props.webAuthnThisDeviceRegistered && 
           <p>This device is already registered, but it is unknown whether for this user.</p>
         }
-        <Modal show={this.state.showDialog} onHide={this.handleDialogClose} >
-          <Modal.Header closeButton>
-            <Modal.Title>Add Login Key</Modal.Title>
-          </Modal.Header>
-          <Form onSubmit={this.handleDialogStore}>
-            <Modal.Body>
+        <Collapse in={ this.state.showDialog } >
+          <Col xl={{ span: 6 }} lg={{ span: 10 }} md={{ span: 8 }} sm={{ span: 10 }} xs={{ span:11 }} >
+            <p><CloseButton onClick={ this.handleDialogClose } /></p>
+            <h4>Add Login Key</h4>
+            <Form onSubmit={ this.handleDialogStore }>
               <Form.Group controlId="webAuthnFormUsername">
                 <Form.Label>Username (not necessarily your current username - only shown locally in authentication dialog)</Form.Label>
                 <Form.Control type="text" autoFocus placeholder="Enter Username" name="username" onChange={this.handleGenericChange} required />
               </Form.Group>
               <Form.Group controlId="webAuthnFormDeviceName">
                 <Form.Label>Device name to identify this device</Form.Label>
-                <Form.Control type="text" autoFocus placeholder="Enter Device Name" name="devicename" onChange={this.handleGenericChange} required />
+                <Form.Control type="text" placeholder="Enter Device Name" name="devicename" onChange={this.handleGenericChange} required />
               </Form.Group>
               <Form.Group controlId="webAuthnFormPassword">
                 <Form.Label>Password (your current password)</Form.Label>
                 <PasswordInputWithToggle onChange={this.handleGenericChange} value={this.state.password} required />
               </Form.Group>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={this.handleDialogClose}>
-                Close
-              </Button>
               <Button variant="primary" type="submit">
                 Save Changes
               </Button>
-            </Modal.Footer>
-          </Form>
-        </Modal>
+            </Form>
+          </Col>
+        </Collapse>
         {this.props.webAuthnDevices.length > 0 &&
-          <div>
+          <div className={styles.WebAuthnList}>
             <h4>Stored devices</h4>
             <DataTable noHeader columns={this.state.columns} data={this.props.webAuthnDevices} dense pagination striped/>
           </div>
