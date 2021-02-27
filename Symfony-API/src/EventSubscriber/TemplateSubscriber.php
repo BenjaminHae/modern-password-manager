8a4bad4825cd563d3532b74301090140a6a4c7e7
Benjamin Häublein <benjaminh@debian.vm.hp>
2021-02-27 10:57:52 +0100

    add headers

---
+++ Symfony-API/src/EventSubscriber/TemplateSubscriber.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Bundle\FrameworkBundle\Controller\TemplateController;
+use Symfony\Component\HttpFoundation\HeaderUtils;
+
+
+class TemplateSubscriber implements EventSubscriberInterface
+{
+    public function __construct()
+    {
+    }
+
+    public function onKernelController(ControllerEvent $event)
+    {
+        //$controller = $event->getController();
+
+        //// when a controller class defines multiple action methods, the controller
+        //// is returned as [$controllerInstance, 'methodName']
+        //if (is_array($controller)) {
+        //    $controller = $controller[0];
+        //}
+
+        //if ($controller instanceof Symfony\Bundle\FrameworkBundle\Controller\TemplateController) {
+        //    // do session!
+        //}
+    }
+
+    public function onKernelResponse(ResponseEvent $event)
+    {
+        $response = $event->getResponse();
+        $ct = $response->headers->get('content-type', "text/html");
+        if ($ct === "text/html") {
+            $response->headers->set("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; object-src 'none'; frame-ancestors 'none';");
+            $response->headers->set("Referrer-Policy", "no-referrer");
+        }
+        else {
+            $disposition = $response->headers->makeDisposition(HeaderUtils::DISPOSITION_ATTACHMENT, "data.json");
+            $response->headers->set('Content-Disposition', $disposition);
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::CONTROLLER => 'onKernelController',
+            KernelEvents::RESPONSE => 'onKernelResponse',
+        ];
+    }
+}
+?>
