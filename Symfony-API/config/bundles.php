a3e2a83df956d39db9adec8fa80e4e7d4c884db2
Benjamin Häublein <benjaminh@debian.vm.hp>
2021-02-28 09:56:19 +0100

    install monolog

--- Symfony-API/config/bundles.php
+++ Symfony-API/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
