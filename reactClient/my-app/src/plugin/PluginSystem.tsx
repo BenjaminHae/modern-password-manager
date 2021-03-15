d90c30607e1808605d2e61020b77adc4416f0945
Benjamin Häublein <benjaminh@debian.vm.hp>
2021-03-15 18:40:28 +0100

    add query for CredentialSources

--- reactClient/my-app/src/plugin/PluginSystem.tsx
+++ reactClient/my-app/src/plugin/PluginSystem.tsx
@@ -7,6 +7,7 @@ import activatedPlugins from './ActivatedPlugins';
 import * as BasePlugin from './BasePlugin';
 import { IMessageOptions } from '../libs/MessageManager';
 import ShortcutManager from '../libs/ShortcutManager';
+import { ICredentialSource, instanceOfCredentialSource } from '../libs/CredentialSource';
 
 export type AccountsFilter = (accounts: Array<Account>) => Array<Account>;
 type AccountFilter = (account: Account) => boolean;
@@ -26,6 +27,7 @@ export class PluginSystem {
   filters: { [index: string]: AccountFilter } = {};
   filterPresent = false;
   
+  credentialSources: Array<ICredentialSource> = [];
   mainViewCallback: Array<() => JSX.Element | void> = [];
   resetFilterCallback: Array<() => void> = [];
   accountsReadyCallback: Array<(accounts: Array<Account>) => void> = [];
@@ -108,6 +110,13 @@ export class PluginSystem {
     if (BasePlugin.instanceOfIPluginWithAccountListShortcuts(plugin)) {
       this.accountListShortcutsCallback.push(plugin.accountListShortcuts.bind(plugin));
     }
+    if (instanceOfCredentialSource(plugin)) {
+      this.credentialSources.push(plugin);
+    }
+  }
+
+  getCredentialSources(): Array<ICredentialSource> {
+    return this.credentialSources;
   }
 
   registerAuthenticatedUIHandler(handler: IAuthenticatedUIHandler): void {
