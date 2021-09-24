41d1bcaf414b26f8e0b5d09140eb1d69346d6956
Benjamin Häublein <benjaminh@debian.vm.hp>
2021-09-24 20:43:34 +0200

    outsourcing script

---
+++ drone_scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+apk update
+apk add zip openssh-client
+# setup ssh
+mkdir ~/.ssh
+echo ${SSH_PRIVATE_KEY} | base64 -d > ~/.ssh/id_drone
+chmod 0600 ~/.ssh/id_drone
+echo ${SSH_HOST_KEY} | base64 -d > ~/.ssh/known_hosts
+chmod 0600 ~/.ssh/known_hosts
+# create zip archive
+(cd ./Symfony-API && zip -q --symlinks -r ../mpm.zip .)
+# transfer to host
+scp -i ~/.ssh/id_drone mpm.zip "${SSH_USER}@${SSH_HOST}:${COPY_REMOTE_PATH}"
+# extract to destination
+ssh -i ~/.ssh/id_drone ${SSH_USER}@${SSH_HOST} "unzip -o -q ${COPY_PATH} -d ${DEPLOY_PATH} && rm ${COPY_PATH}" > test.txt
+# execute db update
+ssh -i ~/.ssh/id_drone "${SSH_USER}@${SSH_HOST}" "cd ${DEPLOY_PATH} && ${PHP_VERSION} bin/console doctrine:schema:update --dump-sql && ${PHP_VERSION} bin/console doctrine:schema:update --force && ${PHP_VERSION} bin/console cache:clear"
