44591851c0a08bd9add3013833d9448c26f8a878
Benjamin Haeublein <benjaminh@testing-vm.lan.benjaminh.de>
2022-02-17 10:17:47 +0100

    update configuration for Symfony6

--- Symfony-API/config/bundles.php
+++ Symfony-API/config/bundles.php
@@ -2,9 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
-    OpenAPI\Server\OpenAPIServerBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
@@ -12,4 +9,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    OpenAPI\Server\OpenAPIServerBundle::class => ['all' => true],
 ];
